a5adc13bfc779cf8bc24d6e21f0dde9aad6dc131
Marcos Sakaguchi <marcos.sakaguchi@ciag.org.br>
2023-03-24 14:12:53 -0300

    feat: starting Expo project

---
+++ src/components/Texto.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text,StyleSheet} from 'react-navive';
+
+
+export default function Texto({children}){
+return <Text> {children}</Text>
+}
+const estilos = StyleSheet.create({
+
+})
